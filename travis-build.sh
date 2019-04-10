4e6069efdab4ba96fdc435da2c4e3e9917236e55
graemerocher <graeme.rocher@gmail.com>
2019-04-10 13:59:10 +0200

    Back to snapshot

--- travis-build.sh
+++ travis-build.sh
@@ -70,9 +70,6 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
       ./gradlew --stop
      if [[ -n $TRAVIS_TAG ]]; then
          ./gradlew bintrayUpload --no-daemon --stacktrace || EXIT_STATUS=$?
-         if [[ $EXIT_STATUS -eq 0 ]]; then
-           ./gradlew synchronizeWithMavenCentral --no-daemon
-         fi
      else
          ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
      fi
@@ -120,6 +117,12 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
         cd ..
 
         rm -rf gh-pages
+
+        if [[ -n $TRAVIS_TAG ]]; then
+          if [[ $EXIT_STATUS -eq 0 ]]; then
+             ./gradlew synchronizeWithMavenCentral --no-daemon
+          fi
+        fi
       fi
    fi
 fi
