a83dfa2989261bbd0fd852d125104e38925fca2f
graemerocher <graeme.rocher@gmail.com>
2019-04-15 17:16:01 +0200

    Remove old examples

--- examples/api-proxy-graal-custom-runtime-example/invoke.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p build
-aws lambda invoke --function-name micronaut-runtime --payload '{"resource": "/{proxy+}", "path": "/ping", "httpMethod": "GET"}' build/response.txt
-echo "RESPONSE:"
-echo "---------"
-cat build/response.txt
