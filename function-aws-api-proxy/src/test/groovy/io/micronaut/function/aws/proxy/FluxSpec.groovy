a24b5f5303cffd72a06f06cb0e545c8fec885d44
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-01-22 13:10:16 +0100

    Check whether the route result is a single, and if not, call toList() on the Flowable.
    Fixes #51

---
+++ function-aws-api-proxy/src/test/groovy/io/micronaut/function/aws/proxy/FluxSpec.groovy
@@ -0,0 +1,44 @@
+package io.micronaut.function.aws.proxy
+
+import com.amazonaws.serverless.proxy.internal.testutils.AwsProxyRequestBuilder
+import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext
+import com.amazonaws.services.lambda.runtime.Context
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpMethod
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import reactor.core.publisher.Flux
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+class FluxSpec extends Specification {
+
+    @Shared @AutoCleanup MicronautLambdaContainerHandler handler = MicronautLambdaContainerHandler.getAwsProxyHandler(
+            ApplicationContext.build()
+    )
+    @Shared Context lambdaContext = new MockLambdaContext()
+
+    void "test getAll method"() {
+        given:
+        AwsProxyRequestBuilder builder = new AwsProxyRequestBuilder("/users", HttpMethod.GET.toString())
+
+        when:
+        def response = handler.proxy(builder.build(), lambdaContext)
+
+        then:
+        response.statusCode == 200
+        response.body == '["Joe","Lewis"]'
+    }
+
+
+    @Controller("/users")
+    static class UserController {
+
+        @Get
+        Flux<String> getAll() {
+            return Flux.fromIterable(["Joe", "Lewis"])
+        }
+
+    }
+}
