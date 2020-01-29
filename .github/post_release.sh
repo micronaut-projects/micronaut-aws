cc639e16345541badfa89c5d9e4ca69d356d22ef
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-01-29 15:01:33 +0100

    Moving some steps to a pre-release phase

--- .github/post_release.sh
+++ .github/post_release.sh
@@ -14,23 +14,6 @@ target_branch=`cat $GITHUB_EVENT_PATH | jq '.release.target_commitish' | sed -e 
 echo $target_branch
 git checkout $target_branch
 
-echo "Setting release version in gradle.properties"
-sed -i "s/^projectVersion.*$/projectVersion\=${3}/" gradle.properties
-cat gradle.properties
-
-echo "Pushing release version and recreating v${3} tag"
-git add gradle.properties
-git commit -m "Release v${3}"
-git push origin $target_branch
-git push origin :refs/tags/v${3}
-git tag -fa v${3} -m "Release v${3}"
-git push origin $target_branch --tags
-
-echo "Closing again the release after updating the tag"
-release_url=`cat $GITHUB_EVENT_PATH | jq '.release.url' | sed -e 's/^"\(.*\)"$/\1/g'`
-echo $release_url
-curl -s --request PATCH -H "Authorization: Bearer $1" -H "Content-Type: application/json" $release_url --data "{\"draft\": false}"
-
 echo -n "Retrieving current milestone number: "
 milestone_number=`curl -s https://api.github.com/repos/$2/milestones | jq -c ".[] | select (.title == \"$3\") | .number" | sed -e 's/"//g'`
 echo $milestone_number
