d05ee7be479d4120213df568bac9ead0b295b370
Álvaro Sánchez-Mariscal <alvarosanchez@users.noreply.github.com>
2020-02-19 12:43:53 +0100

    Using a different ObjectMapper if configured to do so (#195)
    * Unrolling properties so that dependabot can pick them up
    
    * Adding missing headers
    
    * Using a different ObjectMapper if configured to do so. Fixes #186
    
    * This will go into 1.4.0
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle

---
+++ function-aws-api-proxy/src/test/groovy/io/micronaut/function/aws/proxy/ObjectMapperListenerSpec.groovy
@@ -0,0 +1,56 @@
+package io.micronaut.function.aws.proxy
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Requires
+import io.micronaut.context.event.BeanCreatedEvent
+import io.micronaut.context.event.BeanCreatedEventListener
+import io.micronaut.inject.qualifiers.Qualifiers
+import spock.lang.Issue
+import spock.lang.Specification
+
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Issue("https://github.com/micronaut-projects/micronaut-aws/issues/186")
+class ObjectMapperListenerSpec extends Specification {
+
+    void "the aws object mapper can be customised" () {
+        given:
+        MicronautLambdaContainerHandler handler = new MicronautLambdaContainerHandler(
+                ApplicationContext.build().properties([
+                        'spec.name': 'ObjectMapperListenerSpec',
+                        'jackson.property-naming-strategy': 'SNAKE_CASE',
+                        'aws.proxy.shared-object-mapper': false
+                ])
+        )
+
+        when:
+        ObjectMapper global = handler.applicationContext.getBean(ObjectMapper)
+        ObjectMapper aws = handler.applicationContext.getBean(ObjectMapper, Qualifiers.byName("aws"))
+
+        then:
+        global.deserializationConfig.propertyNamingStrategy == PropertyNamingStrategy.SNAKE_CASE
+        aws.deserializationConfig.propertyNamingStrategy == PropertyNamingStrategy.UPPER_CAMEL_CASE
+    }
+
+    @Singleton
+    @Requires(property = 'spec.name', value = 'ObjectMapperListenerSpec')
+    static class ObjectMapperListener implements BeanCreatedEventListener<ObjectMapper> {
+
+        @Override
+        ObjectMapper onCreated(BeanCreatedEvent<ObjectMapper> event) {
+            ObjectMapper bean = event.bean
+            event.getBeanDefinition()
+                    .findAnnotation(Named.class)
+                    .map{ n -> n.getValues().get("value") as String }
+                    .filter{v -> v == "aws"}
+                    .ifPresent{
+                        bean.propertyNamingStrategy = PropertyNamingStrategy.UPPER_CAMEL_CASE
+                    }
+            return bean
+        }
+    }
+
+}
---
+++ function-aws-api-proxy/src/test/groovy/io/micronaut/function/aws/proxy/ObjectMapperSpec.groovy
@@ -0,0 +1,69 @@
+package io.micronaut.function.aws.proxy
+
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.exceptions.NoSuchBeanException
+import io.micronaut.inject.qualifiers.Qualifiers
+import spock.lang.Issue
+import spock.lang.Specification
+
+@Issue("https://github.com/micronaut-projects/micronaut-aws/issues/186")
+class ObjectMapperSpec extends Specification {
+
+    void "by default, the object mapper is shared" () {
+        given:
+        MicronautLambdaContainerHandler handler = new MicronautLambdaContainerHandler(ApplicationContext.build())
+
+        when:
+        ObjectMapper objectMapper = handler.applicationContext.getBean(ObjectMapper)
+
+        then:
+        objectMapper.deserializationConfig.propertyNamingStrategy == null
+
+        when:
+        handler.applicationContext.getBean(ObjectMapper, Qualifiers.byName("aws"))
+
+        then:
+        thrown(NoSuchBeanException)
+    }
+
+    void "when changing the global object mapper configuration, by default it is still shared"() {
+        given:
+        MicronautLambdaContainerHandler handler = new MicronautLambdaContainerHandler(
+                ApplicationContext.build().properties([
+                        'jackson.property-naming-strategy': 'SNAKE_CASE'
+                ])
+        )
+
+        expect:
+        handler.applicationContext.getBean(ObjectMapper) != null
+
+        when:
+        handler.applicationContext.getBean(ObjectMapper, Qualifiers.byName("aws"))
+
+        then:
+        thrown(NoSuchBeanException)
+    }
+
+
+    void "when changing global object mapper configuration, it can be configured to create a new one for aws" () {
+        given:
+        MicronautLambdaContainerHandler handler = new MicronautLambdaContainerHandler(
+                ApplicationContext.build().properties([
+                        'jackson.property-naming-strategy': 'SNAKE_CASE',
+                        'aws.proxy.shared-object-mapper': false
+                ])
+        )
+
+        when:
+        ObjectMapper global = handler.applicationContext.getBean(ObjectMapper)
+        ObjectMapper aws = handler.applicationContext.getBean(ObjectMapper, Qualifiers.byName("aws"))
+
+        then:
+        global.deserializationConfig.propertyNamingStrategy == PropertyNamingStrategy.SNAKE_CASE
+        aws.deserializationConfig.propertyNamingStrategy == null
+    }
+
+}
