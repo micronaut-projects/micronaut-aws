beba3781a6c7449f5532ea533e1440d156e0c8ec
Sergio del Amo <sergio.delamo@softamo.com>
2020-12-05 06:19:27 +0100

    fix: request parameter gathering in AWS proxy adapter
    close:  https://github.com/micronaut-projects/micronaut-aws/issues/754

--- function-aws-api-proxy-test/src/test/groovy/io/micronaut/function/aws/proxy/test/AwsApiProxyTestServerSpec.groovy
+++ function-aws-api-proxy-test/src/test/groovy/io/micronaut/function/aws/proxy/test/AwsApiProxyTestServerSpec.groovy
@@ -6,9 +6,10 @@ import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.QueryValue
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
-import io.micronaut.test.annotation.MicronautTest
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import spock.lang.Specification
 
 import javax.inject.Inject
@@ -21,41 +22,46 @@ class AwsApiProxyTestServerSpec extends Specification {
 
     void 'test invoke function via server'() {
         when:
-        def result = client.retrieve('/test').blockingFirst()
+        String result = client.retrieve('/test').blockingFirst()
 
         then:
         result == 'good'
     }
-
-
-    void 'test invoke function via server 2'() {
-        when:
-        def result = client.retrieve('/test').blockingFirst()
-
-        then:
-        result == 'good'
-    }
-
+    
     void 'test invoke post via server'() {
         when:
-        def result = client.retrieve(HttpRequest.POST('/test', "body")
+        String result = client.retrieve(HttpRequest.POST('/test', "body")
                                         .contentType(MediaType.TEXT_PLAIN), String).blockingFirst()
 
         then:
         result == 'goodbody'
     }
 
+    void 'query values are picked up'() {
+        when:
+        String result = client.retrieve(HttpRequest.GET('/test-param?foo=bar')
+                                        .contentType(MediaType.TEXT_PLAIN), String).blockingFirst()
 
-    @Controller('/test')
+        then:
+        result == 'get:bar'
+    }
+
+
+    @Controller
     static class TestController {
-        @Get(value = '/', produces = MediaType.TEXT_PLAIN)
+        @Get(value = '/test', produces = MediaType.TEXT_PLAIN)
         String test() {
-            return 'good'
+            'good'
         }
 
-        @Post(value = '/', processes = MediaType.TEXT_PLAIN)
+        @Post(value = '/test', processes = MediaType.TEXT_PLAIN)
         String test(@Body String body) {
-            return 'good' + body
+            'good' + body
+        }
+
+        @Get(value = '/test-param{?foo}', processes = MediaType.TEXT_PLAIN)
+        String search(@QueryValue String foo) {
+            'get:' + foo
         }
     }
 }
