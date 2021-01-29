6154e233f3a92a2a047a91ab47740560697f8fb8
ttzn <7677876+ttzn@users.noreply.github.com>
2021-01-29 17:50:28 +0100

    Improve MicronautLambdaTest implementation (#889)
    * upgrade micronaut-test dependency to 2.3.2 and take advantage of code reuse improvements
    * add contextBuilder option to MicronautLambdaTest (with caveat)

--- function-aws-test/src/test/java/io/micronaut/function/aws/test/MicronautLambdaExtensionTest.java
+++ function-aws-test/src/test/java/io/micronaut/function/aws/test/MicronautLambdaExtensionTest.java
@@ -3,11 +3,16 @@ package io.micronaut.function.aws.test;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.BeanRegistration;
 import io.micronaut.function.aws.test.annotation.MicronautLambdaTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @MicronautLambdaTest
 public class MicronautLambdaExtensionTest {
@@ -17,10 +22,10 @@ public class MicronautLambdaExtensionTest {
     @Test
     public void testContextProperlyConfigured() {
         Set<String> expectedNames = new HashSet<>(Arrays.asList("test", "function", "lambda"));
-        Assertions.assertNotNull(context);
-        Assertions.assertEquals(context.getEnvironment().getActiveNames(), expectedNames);
+        assertNotNull(context);
+        assertEquals(context.getEnvironment().getActiveNames(), expectedNames);
         Collection<BeanRegistration<EagerSingleton>> registrations =
                 context.getActiveBeanRegistrations(EagerSingleton.class);
-        Assertions.assertEquals(1, registrations.size());
+        assertEquals(1, registrations.size());
     }
 }
