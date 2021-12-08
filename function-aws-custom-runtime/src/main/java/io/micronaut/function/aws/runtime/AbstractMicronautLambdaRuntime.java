e2a9e90ca752fa76616a45152d65983225931ce2
Sergio del Amo <sergio.delamo@softamo.com>
2021-12-08 12:58:17 +0100

    Revert "feature: Revert "Send a user-agent header with requests to the Lambda Runtime API" (#1238)"
    This reverts commit 9294516c6ca9407d62718ea92537e4386d2206e6.

--- function-aws-custom-runtime/src/main/java/io/micronaut/function/aws/runtime/AbstractMicronautLambdaRuntime.java
+++ function-aws-custom-runtime/src/main/java/io/micronaut/function/aws/runtime/AbstractMicronautLambdaRuntime.java
@@ -61,6 +61,8 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
 
+import static io.micronaut.http.HttpHeaders.USER_AGENT;
+
 /**
  * Class that can be used as a entry point for a AWS Lambda custom runtime.
  * @see <a href="https://docs.aws.amazon.com/lambda/latest/dg/runtimes-custom.html">Custom AWS Lambda runtimes</a>.
@@ -98,6 +100,11 @@ import java.util.function.Predicate;
 )
 public abstract class AbstractMicronautLambdaRuntime<RequestType, ResponseType, HandlerRequestType, HandlerResponseType>
         implements ApplicationContextProvider, AwsLambdaRuntimeApi {
+
+    static final String USER_AGENT_VALUE = String.format(
+            "micronaut/%s-%s",
+            System.getProperty("java.vendor.version"),
+            AbstractMicronautLambdaRuntime.class.getPackage().getImplementationVersion());
 
     protected Object handler;
 
@@ -330,7 +337,7 @@ public abstract class AbstractMicronautLambdaRuntime<RequestType, ResponseType, 
             try {
                 while (loopUntil.test(runtimeApiURL)) {
                     final HttpResponse<RequestType> response = blockingHttpClient.exchange(
-                            HttpRequest.GET(AwsLambdaRuntimeApi.NEXT_INVOCATION_URI), Argument.of(requestType));
+                            HttpRequest.GET(AwsLambdaRuntimeApi.NEXT_INVOCATION_URI).header(USER_AGENT, USER_AGENT_VALUE), Argument.of(requestType));
                     final RequestType request = response.body();
                     if (request != null) {
                         logn(LogLevel.DEBUG, "request body ", request);
