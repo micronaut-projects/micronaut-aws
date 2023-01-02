0c2785a07e47579e0f594467b348283f06650c28
Sergio del Amo <sergio.delamo@softamo.com>
2023-01-02 16:15:24 +0100

    simpler module

--- test-suite-http-server-tck-function-aws-api-proxy/build.gradle.kts
+++ test-suite-http-server-tck-function-aws-api-proxy/build.gradle.kts
@@ -1,13 +1,6 @@
 plugins {
-    id("io.micronaut.build.internal.http-test-module")
+    id("io.micronaut.build.internal.aws.http-server-tck-module")
 }
-repositories {
-    mavenCentral()
-}
-val micronautVersion: String by project
 dependencies {
     testImplementation(projects.functionAwsApiProxy)
-    testImplementation(mn.micronaut.http.server.tck)
-    testImplementation(mn.micronaut.http.client)
-    testImplementation(libs.junit.platform.engine)
 }
