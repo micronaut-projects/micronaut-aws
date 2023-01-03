9113e26d247955cb17d79fd0dc030ca6cea3094e
Sergio del Amo <sergio.delamo@softamo.com>
2023-01-03 15:30:18 +0100

    BREAKING: remove deprecated code (#1561)

--- aws-sdk-v2/src/main/java/io/micronaut/aws/sdk/v2/service/sns/SnsClientFactory.java
+++ aws-sdk-v2/src/main/java/io/micronaut/aws/sdk/v2/service/sns/SnsClientFactory.java
@@ -22,7 +22,6 @@ import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.annotation.Nullable;
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
@@ -42,34 +41,6 @@ import software.amazon.awssdk.services.sns.SnsClientBuilder;
  */
 @Factory
 public class SnsClientFactory extends AwsClientFactory<SnsClientBuilder, SnsAsyncClientBuilder, SnsClient, SnsAsyncClient> {
-
-    /**
-     * Constructor.
-     *
-     * @param credentialsProvider The credentials provider
-     * @param regionProvider      The region provider
-     * @deprecated Use {@link SnsClientFactory(AwsCredentialsProviderChain,AwsRegionProviderChain,UserAgentProvider, AWSServiceConfiguration)} instead.
-     */
-    @Deprecated
-    protected SnsClientFactory(AwsCredentialsProviderChain credentialsProvider, AwsRegionProviderChain regionProvider) {
-        super(credentialsProvider, regionProvider, null);
-    }
-
-    /**
-     * Constructor.
-     *
-     * @param credentialsProvider The credentials provider
-     * @param regionProvider      The region provider
-     * @param userAgentProvider User-Agent Provider
-     * @deprecated Use {@link SnsClientFactory(AwsCredentialsProviderChain,AwsRegionProviderChain,UserAgentProvider, AWSServiceConfiguration)} instead.
-     */
-    @Deprecated
-    protected SnsClientFactory(AwsCredentialsProviderChain credentialsProvider,
-                               AwsRegionProviderChain regionProvider,
-                               @Nullable UserAgentProvider userAgentProvider) {
-        super(credentialsProvider, regionProvider, userAgentProvider);
-    }
-
     /**
      * Constructor.
      *
@@ -79,7 +50,6 @@ public class SnsClientFactory extends AwsClientFactory<SnsClientBuilder, SnsAsyn
      * @param awsServiceConfiguration  AWS Service Configuration
 
      */
-    @Inject
     protected SnsClientFactory(AwsCredentialsProviderChain credentialsProvider,
                                AwsRegionProviderChain regionProvider,
                                @Nullable UserAgentProvider userAgentProvider,
