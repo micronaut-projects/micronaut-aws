0947eb2a41d15a4806e9934c4f05d58053851fa2
Tim Yates <tim.yates@gmail.com>
2023-04-17 12:30:27 +0100

    Add test-suite-http-server-tck-function-aws-api-proxy-payloadv2 to project [skip ci]

--- test-suite-http-server-tck-function-aws-api-gateway-proxy-payloadv1/build.gradle.kts
+++ test-suite-http-server-tck-function-aws-api-gateway-proxy-payloadv1/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("io.micronaut.build.internal.aws.http-server-tck-module")
 }
+
 dependencies {
     testImplementation(projects.micronautFunctionAwsApiGatewayProxy)
     testImplementation(mnReactor.micronaut.reactor)
