fb4aa79fc842a492f3e51b45f454beb5a690a61d
Tim Yates <tim.yates@gmail.com>
2023-05-05 17:01:38 +0100

    Rename test classes so I stop being confused

---
+++ test-suite-http-server-tck-function-aws-api-gateway-proxy-payloadv2/src/test/java/io/micronaut/http/server/tck/lambda/tests/FunctionAwsApiGatewayProxyV2HttpServerTestSuite.java
@@ -0,0 +1,11 @@
+package io.micronaut.http.server.tck.lambda.tests;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SelectPackages("io.micronaut.http.server.tck.tests")
+@SuiteDisplayName("HTTP Server TCK for Function AWS API Gateway Proxy v2 Event model")
+public class FunctionAwsApiGatewayProxyV2HttpServerTestSuite {
+}
