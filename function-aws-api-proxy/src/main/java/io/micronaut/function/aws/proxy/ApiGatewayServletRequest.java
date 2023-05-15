618efbbc733fb96d01bd2cf3ea689eb824bfba82
Tim Yates <tim.yates@gmail.com>
2023-05-15 15:27:23 +0100

    Switch ApiGatewayServletRequest body to work like Servlet

--- function-aws-api-proxy/src/main/java/io/micronaut/function/aws/proxy/ApiGatewayServletRequest.java
+++ function-aws-api-proxy/src/main/java/io/micronaut/function/aws/proxy/ApiGatewayServletRequest.java
@@ -15,32 +15,45 @@
  */
 package io.micronaut.function.aws.proxy;
 
+import io.micronaut.core.annotation.AnnotationMetadata;
 import io.micronaut.core.annotation.Internal;
 import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.core.convert.ConversionService;
-import io.micronaut.core.convert.value.ConvertibleValues;
 import io.micronaut.core.convert.value.MutableConvertibleValues;
 import io.micronaut.core.convert.value.MutableConvertibleValuesMap;
+import io.micronaut.core.io.buffer.ByteBuffer;
 import io.micronaut.core.type.Argument;
+import io.micronaut.core.util.CollectionUtils;
+import io.micronaut.core.util.SupplierUtil;
+import io.micronaut.http.HttpAttributes;
 import io.micronaut.http.HttpMethod;
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.MutableHttpRequest;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.codec.CodecException;
 import io.micronaut.http.codec.MediaTypeCodec;
 import io.micronaut.http.codec.MediaTypeCodecRegistry;
 import io.micronaut.http.cookie.Cookie;
 import io.micronaut.http.cookie.Cookies;
+import io.micronaut.inject.ExecutionHandle;
+import io.micronaut.json.codec.MapperMediaTypeCodec;
+import io.micronaut.json.tree.JsonNode;
 import io.micronaut.servlet.http.MutableServletHttpRequest;
 import io.micronaut.servlet.http.ServletExchange;
 import io.micronaut.servlet.http.ServletHttpRequest;
+import io.micronaut.web.router.RouteMatch;
 
 import java.io.BufferedReader;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Supplier;
 
 /**
  * Base class for all the versions of the Gateway events.
@@ -52,7 +65,8 @@ import java.util.Optional;
 @Internal
 public abstract class ApiGatewayServletRequest<T, REQ, RES> implements MutableServletHttpRequest<REQ, T>, ServletExchange<REQ, RES> {
 
-    protected Object body;
+    private static final Set<Class<?>> RAW_BODY_TYPES = CollectionUtils.setOf(String.class, byte[].class, ByteBuffer.class, InputStream.class);
+
     protected ConversionService conversionService;
     protected final REQ requestEvent;
     private URI uri;
@@ -60,6 +74,7 @@ public abstract class ApiGatewayServletRequest<T, REQ, RES> implements MutableSe
     private Cookies cookies;
     private final MediaTypeCodecRegistry codecRegistry;
     private MutableConvertibleValues<Object> attributes;
+    private Supplier<Optional<T>> body;
 
     protected ApiGatewayServletRequest(ConversionService conversionService, MediaTypeCodecRegistry codecRegistry, REQ request, URI uri, HttpMethod httpMethod) {
         this.conversionService = conversionService;
@@ -67,6 +82,87 @@ public abstract class ApiGatewayServletRequest<T, REQ, RES> implements MutableSe
         this.requestEvent = request;
         this.uri = uri;
         this.httpMethod = httpMethod;
+        this.body = SupplierUtil.memoizedNonEmpty(() -> {
+            T built = (T) buildBody();
+            return Optional.ofNullable(built);
+        });
+    }
+
+    @Nullable
+    protected Object buildBody() {
+        final MediaType contentType = getContentType().orElse(MediaType.APPLICATION_JSON_TYPE);
+        if (isFormSubmission(contentType)) {
+            return getParameters().asMap();
+        } else {
+            if (getContentLength() == 0) {
+                return null;
+            }
+            Argument<?> resolvedBodyType = resolveBodyType();
+            try (InputStream inputStream = getInputStream())  {
+                if (resolvedBodyType != null && RAW_BODY_TYPES.contains(resolvedBodyType.getType())) {
+                    return inputStream.readAllBytes();
+                } else {
+                    final MediaTypeCodec codec = codecRegistry.findCodec(contentType).orElse(null);
+                    if (contentType.equals(MediaType.APPLICATION_JSON_TYPE) && codec instanceof MapperMediaTypeCodec mapperCodec) {
+                        return readJson(inputStream, mapperCodec);
+                    } else if (codec != null) {
+                        return decode(inputStream, codec);
+                    } else {
+                        return inputStream.readAllBytes();
+                    }
+                }
+            } catch (EOFException e) {
+                // no content
+                return null;
+            } catch (IOException e) {
+                throw new CodecException("Error decoding request body: " + e.getMessage(), e);
+            }
+        }
+    }
+
+    private Argument<?> resolveBodyType() {
+        RouteMatch<?> route = this.getAttribute(HttpAttributes.ROUTE_MATCH, RouteMatch.class).orElse(null);
+        if (route != null) {
+            Argument<?> bodyType = route.getRouteInfo().getFullRequestBodyType()
+                /*
+                The getBodyArgument() method returns arguments for functions where it is
+                not possible to dictate whether the argument is supposed to bind the entire
+                body or just a part of the body. We check to ensure the argument has the body
+                annotation to exclude that use case
+                */
+                .filter(argument -> {
+                    AnnotationMetadata annotationMetadata = argument.getAnnotationMetadata();
+                    if (annotationMetadata.hasAnnotation(Body.class)) {
+                        return annotationMetadata.stringValue(Body.class).isEmpty();
+                    } else {
+                        return false;
+                    }
+                })
+                .orElseGet(() -> {
+                    if (route instanceof ExecutionHandle<?, ?> handle) {
+                        for (Argument<?> argument : handle.getArguments()) {
+                            if (argument.getType() == HttpRequest.class) {
+                                return argument;
+                            }
+                        }
+                    }
+                    return Argument.OBJECT_ARGUMENT;
+                });
+            if (bodyType.getType() == HttpRequest.class) {
+                bodyType = bodyType.getFirstTypeVariable().orElse(Argument.OBJECT_ARGUMENT);
+            }
+            return bodyType;
+        } else {
+            return Argument.OBJECT_ARGUMENT;
+        }
+    }
+
+    private Object decode(InputStream inputStream, MediaTypeCodec codec) throws IOException {
+        return codec.decode(Argument.of(byte[].class), inputStream);
+    }
+
+    private Object readJson(InputStream inputStream, MapperMediaTypeCodec mapperCodec) throws IOException {
+        return mapperCodec.getJsonMapper().readValue(inputStream, Argument.of(JsonNode.class));
     }
 
     @Override
@@ -126,62 +222,21 @@ public abstract class ApiGatewayServletRequest<T, REQ, RES> implements MutableSe
         return attributes;
     }
 
+    @NonNull
     @Override
     public Optional<T> getBody() {
-        return (Optional<T>) getBody(Argument.OBJECT_ARGUMENT);
+        return this.body.get();
     }
 
+    @NonNull
     @Override
     public <B> Optional<B> getBody(Argument<B> arg) {
-        if (arg != null) {
-            final Class<B> type = arg.getType();
-            final MediaType contentType = getContentType().orElse(MediaType.APPLICATION_JSON_TYPE);
-            if (body == null) {
-
-                if (isFormSubmission(contentType)) {
-                    body = getParameters();
-                    if (ConvertibleValues.class == type || Object.class == type) {
-                        return (Optional<B>) Optional.of(body);
-                    } else {
-                        return Optional.empty();
-                    }
-                } else {
-
-                    final MediaTypeCodec codec = codecRegistry.findCodec(contentType, type).orElse(null);
-                    if (codec != null) {
-                        try (InputStream inputStream = getInputStream()) {
-                            if (ConvertibleValues.class == type) {
-                                final Map map = codec.decode(Map.class, inputStream);
-                                body = ConvertibleValues.of(map);
-                                return (Optional<B>) Optional.of(body);
-                            } else {
-                                final B value = codec.decode(arg, inputStream);
-                                body = value;
-                                return Optional.ofNullable(value);
-                            }
-                        } catch (IOException e) {
-                            throw new CodecException("Error decoding request body: " + e.getMessage(), e);
-                        }
-
-                    }
-                }
-            } else {
-                if (type.isInstance(body)) {
-                    return (Optional<B>) Optional.of(body);
-                } else {
-                    final B result = conversionService.convertRequired(body, arg);
-                    return Optional.ofNullable(result);
-                }
-
-            }
-        }
-        return Optional.empty();
+        return getBody().map(t -> conversionService.convertRequired(t, arg));
     }
 
     private boolean isFormSubmission(MediaType contentType) {
-        return MediaType.MULTIPART_FORM_DATA_TYPE.equals(contentType);
+        return MediaType.APPLICATION_FORM_URLENCODED_TYPE.equals(contentType) || MediaType.MULTIPART_FORM_DATA_TYPE.equals(contentType);
     }
-
 
     @Override
     public MutableHttpRequest<T> cookie(Cookie cookie) {
