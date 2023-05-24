cb46cf41d5a32939269e2a692c29bce64580b92e
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-24 22:00:43 +0200

    remove snapshot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,7 +44,6 @@ include("test-suite-graal")
 configure<io.micronaut.build.MicronautBuildSettingsExtension> {
     useStandardizedProjectNames.set(true)
 
-    addSnapshotRepository()
     importMicronautCatalog()
     importMicronautCatalog("micronaut-discovery-client")
     importMicronautCatalog("micronaut-groovy")
