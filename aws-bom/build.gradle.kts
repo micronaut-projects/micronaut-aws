c1168498f877fa1a0f5de712d3732777053123cf
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-26 12:52:29 +0200

    remove aws-serverless-core version

--- aws-bom/build.gradle.kts
+++ aws-bom/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 micronautBom {
     suppressions {
+        acceptedVersionRegressions.add("aws-serverless-core") // removed for 4.0.0
         acceptedLibraryRegressions.add("aws-serverless-core") // removed for 4.0.0
         acceptedLibraryRegressions.add("micronaut-aws-cdk") // moved to starter for 4.0.0
     }
