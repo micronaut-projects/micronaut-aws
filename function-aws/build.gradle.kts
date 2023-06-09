ac06548f7508322bacc8799bc6a8b588be9fbd8f
Sergio del Amo <sergio.delamo@softamo.com>
2023-06-09 09:51:10 +0200

    feat: Add CustomPojoSerializer (#1758)
    * feat: Add CustomPojoSerializer
    
    This PR modifies `micronaut-function-aws` to provide an implementation of `com.amazonaws.services.lambda.runtime.CustomPojoSerializer` which is loaded via SPI. This `CustomPojoSerialization` avoids your Micronaut function to pay a double hit on performances when using a serialization library inside the Lambda function.
    
    * create custom exception

--- function-aws/build.gradle.kts
+++ function-aws/build.gradle.kts
@@ -5,10 +5,11 @@ plugins {
 dependencies {
     api(mn.micronaut.function)
     api(libs.managed.aws.lambda.core)
+    implementation(mn.micronaut.json.core)
     testImplementation(mnMongo.micronaut.mongo.sync)
     testImplementation(platform(libs.testcontainers.bom))
     testImplementation(libs.testcontainers.spock)
     testImplementation(libs.testcontainers.mongodb)
     testImplementation(libs.testcontainers)
-    testRuntimeOnly(mn.micronaut.jackson.databind)
+    testImplementation(projects.micronautAwsLambdaEventsSerde)
 }
