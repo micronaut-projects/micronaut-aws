72ea0ad433267bcd6ed14fcb50faafddc44f4431
Tim Yates <tim.yates@gmail.com>
2023-06-12 13:20:10 +0100

    Fix MissingBodyAnnotation TCK test (#1766)
    None of our other TCK tests POST a POJO as a body (we usually craft json as a string).
    
    This picked up that our conversion factory method (to convert it to an AWS event wasn't
    correctly converting the body back to Json if a POJO was sent.

--- test-suite-http-server-tck-function-aws-api-gateway-proxy-payloadv2/src/test/java/io/micronaut/http/server/tck/lambda/APIGatewayV2HTTPEventFactory.java
+++ test-suite-http-server-tck-function-aws-api-gateway-proxy-payloadv2/src/test/java/io/micronaut/http/server/tck/lambda/APIGatewayV2HTTPEventFactory.java
@@ -21,10 +21,15 @@ import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpHeaders;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpVersion;
+import io.micronaut.http.MediaType;
+import io.micronaut.json.JsonMapper;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 
 /**
  * Factory for creating {@link APIGatewayV2HTTPEvent} v2 instances from {@link HttpRequest} instances.
@@ -35,7 +40,24 @@ public final class APIGatewayV2HTTPEventFactory {
     private APIGatewayV2HTTPEventFactory() {
     }
 
-    public static APIGatewayV2HTTPEvent create(HttpRequest<?> request) {
+    public static APIGatewayV2HTTPEvent create(HttpRequest<?> request, JsonMapper jsonMapper) {
+        Function<Object, String> maybeConvertBody = body -> {
+            // Assume no content type == json
+            boolean mapFromJson = request.getContentType().map(MediaType.APPLICATION_JSON_TYPE::equals).orElse(true);
+            if (body instanceof CharSequence) {
+                return body.toString();
+            } else if (body instanceof byte[]) {
+                return new String(((byte[]) body), request.getCharacterEncoding());
+            } else if (mapFromJson) {
+                try {
+                    return jsonMapper.writeValueAsString(body);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+            return null;
+        };
         return new APIGatewayV2HTTPEvent() {
 
             @Override
@@ -75,7 +97,7 @@ public final class APIGatewayV2HTTPEventFactory {
 
             @Override
             public String getBody() {
-                return request.getBody(Argument.of(String.class)).orElse(null);
+                return request.getBody().map(maybeConvertBody).orElse(null);
             }
         };
     }
