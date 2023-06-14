7227b0366f270ad61f4703004bf746332141deb7
Sergio del Amo <sergio.delamo@softamo.com>
2023-06-14 18:43:42 +0200

    add MnAPIGatewayProxyRequestEventStageResolver

--- aws-apigateway/build.gradle.kts
+++ aws-apigateway/build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
 dependencies {
     compileOnly(mn.micronaut.http)
     compileOnly(projects.micronautFunctionAwsApiProxy)
+    compileOnly(projects.micronautAwsLambdaEvents)
     compileOnly(libs.managed.aws.lambda.events)
+    testImplementation(projects.micronautAwsLambdaEvents)
     testImplementation(projects.micronautFunctionAwsApiProxy)
     testImplementation(libs.managed.aws.lambda.events)
 }
