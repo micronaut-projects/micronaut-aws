2dbe4606250dc0f6153ee20e6a7f54b8bfc89e6e
Sergio del Amo <sergio.delamo@softamo.com>
2023-06-28 14:27:52 +0200

    add test for ALB events (#1786)
    * add test for ALB events
    
    * Fix sonar security bug
    
    ---------
    
    Co-authored-by: Tim Yates <tim.yates@gmail.com>

---
+++ test-suite-aws-lambda-events/src/main/java/io/micronaut/aws/lambda/events/tests/ApplicationLoadBalancerRequestEventTest.java
@@ -0,0 +1,40 @@
+package io.micronaut.aws.lambda.events.tests;
+
+import com.amazonaws.services.lambda.runtime.events.ApplicationLoadBalancerRequestEvent;
+import io.micronaut.aws.lambda.events.FileUtils;
+import io.micronaut.function.aws.JsonMapperCustomPojoSerializer;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ApplicationLoadBalancerRequestEventTest {
+
+    @Test
+    @SuppressWarnings("java:S1313") // IP usage is safe here
+    void testDeserializationOfApplicationLoadBalancerRequestEvent() throws IOException {
+        JsonMapperCustomPojoSerializer serializer = new JsonMapperCustomPojoSerializer();
+        String json = FileUtils.text(this.getClass().getClassLoader(), "albRequest.json").orElse(null);
+        assertNotNull(json);
+        ApplicationLoadBalancerRequestEvent event = assertDoesNotThrow(() -> serializer.fromJson(json, ApplicationLoadBalancerRequestEvent.class));
+        assertEquals("arn:aws:elasticloadbalancing:us-east-1:646307737039:targetgroup/fubar-dev-tg/97533b9b279f7d7f", event.getRequestContext().getElb().getTargetGroupArn());
+        assertEquals("GET", event.getHttpMethod());
+        assertEquals("/", event.getPath());
+        assertEquals(Collections.emptyMap(), event.getQueryStringParameters());
+
+        assertEquals("*/*", event.getHeaders().get("accept"));
+        assertEquals("fubar-dev-alb-376753725.us-east-1.elb.amazonaws.com", event.getHeaders().get("host"));
+        assertEquals("curl/7.88.1", event.getHeaders().get("user-agent"));
+        assertEquals("Root=1-649c0255-6789eb512889f9cd6bc9193c", event.getHeaders().get("x-amzn-trace-id"));
+        assertEquals("80.26.234.66", event.getHeaders().get("x-forwarded-for"));
+        assertEquals("80", event.getHeaders().get("x-forwarded-port"));
+        assertEquals("http", event.getHeaders().get("x-forwarded-proto"));
+        assertEquals("", event.getBody());
+        assertFalse(event.getIsBase64Encoded());
+    }
+}
---
+++ test-suite-aws-lambda-events/src/main/java/io/micronaut/aws/lambda/events/tests/ApplicationLoadBalancerResponseEventTest.java
@@ -0,0 +1,34 @@
+package io.micronaut.aws.lambda.events.tests;
+
+import com.amazonaws.services.lambda.runtime.events.ApplicationLoadBalancerResponseEvent;
+import io.micronaut.aws.lambda.events.FileUtils;
+import io.micronaut.function.aws.JsonMapperCustomPojoSerializer;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class ApplicationLoadBalancerResponseEventTest {
+
+    @Test
+    void testDeserializationOfApplicationLoadBalancerResponseEvent() throws IOException {
+        JsonMapperCustomPojoSerializer serializer = new JsonMapperCustomPojoSerializer();
+        String json = FileUtils.text(this.getClass().getClassLoader(), "albResponse.json").orElse(null);
+        assertNotNull(json);
+        ApplicationLoadBalancerResponseEvent event = assertDoesNotThrow(() -> serializer.fromJson(json, ApplicationLoadBalancerResponseEvent.class));
+        assertEquals(200, event.getStatusCode());
+        assertNull(event.getStatusDescription());
+        assertFalse(event.getIsBase64Encoded());
+        assertEquals("Wed, 28 Jun 2023 09:50:16 GMT", event.getHeaders().get("Date"));
+        assertEquals("application/json", event.getHeaders().get("Content-Type"));
+        assertEquals(Collections.singletonList("Wed, 28 Jun 2023 09:50:16 GMT"), event.getMultiValueHeaders().get("Date"));
+        assertEquals(Collections.singletonList("application/json"), event.getMultiValueHeaders().get("Content-Type"));
+        assertEquals("{\"message\":\"Will it Blend?\"}", event.getBody());
+    }
+}
