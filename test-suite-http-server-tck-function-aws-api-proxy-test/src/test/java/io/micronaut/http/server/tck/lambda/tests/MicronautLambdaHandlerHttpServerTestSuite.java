f81cb6404d60b00e4d23a941e6547bec35a1a52d
Sergio del Amo <sergio.delamo@softamo.com>
2023-08-21 09:28:48 +0200

    test: ignore LocalErrorReadingBodyTest

--- test-suite-http-server-tck-function-aws-api-proxy-test/src/test/java/io/micronaut/http/server/tck/lambda/tests/MicronautLambdaHandlerHttpServerTestSuite.java
+++ test-suite-http-server-tck-function-aws-api-proxy-test/src/test/java/io/micronaut/http/server/tck/lambda/tests/MicronautLambdaHandlerHttpServerTestSuite.java
@@ -8,6 +8,7 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @ExcludeClassNamePatterns({
+    "io.micronaut.http.server.tck.tests.LocalErrorReadingBodyTest", // Binding body different type (e.g. a String in error handler)
     "io.micronaut.http.server.tck.tests.FilterProxyTest" // Immmutable request
 })
 @SuiteDisplayName("HTTP Server TCK for Function AWS API Proxy Test")
