3efaed40c02ac91fe91b5299921d5c4ff401541e
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-23 16:49:41 +0100

    tck exclusions

--- test-suite-http-server-tck-function-aws-api-proxy-test/src/test/java/io/micronaut/http/server/tck/lambda/tests/MicronautLambdaHandlerHttpServerTestSuite.java
+++ test-suite-http-server-tck-function-aws-api-proxy-test/src/test/java/io/micronaut/http/server/tck/lambda/tests/MicronautLambdaHandlerHttpServerTestSuite.java
@@ -11,9 +11,10 @@ import org.junit.platform.suite.api.SuiteDisplayName;
     "io.micronaut.http.server.tck.lambda.tests"
 })
 @ExcludeClassNamePatterns({
+        "io.micronaut.http.server.tck.tests.forms.FormsSubmissionsWithListsTest",
+        "io.micronaut.http.server.tck.tests.filter.options.OptionsFilterTest",
     "io.micronaut.http.server.tck.tests.LocalErrorReadingBodyTest", // Binding body different type (e.g. a String in error handler)
     "io.micronaut.http.server.tck.tests.FilterProxyTest" // Immmutable request
-
 })
 @SuiteDisplayName("HTTP Server TCK for Function AWS API Proxy Test")
 public class MicronautLambdaHandlerHttpServerTestSuite {
