a6bf7376ea046d78461fd88237f49666d87e3a33
Sergio del Amo <sergio.delamo@softamo.com>
2023-12-11 15:39:48 +0100

    add test

--- aws-sdk-v2/src/main/java/io/micronaut/aws/sdk/v2/client/netty/NettyClientConfiguration.java
+++ aws-sdk-v2/src/main/java/io/micronaut/aws/sdk/v2/client/netty/NettyClientConfiguration.java
@@ -44,12 +44,7 @@ public class NettyClientConfiguration extends AWSConfiguration {
      * @return The builder for {@link NettyNioAsyncHttpClient}
      */
     public NettyNioAsyncHttpClient.Builder getBuilder() {
-        ProxyConfiguration proxyConfig = proxy.build();
-        if (proxyConfig.host() == null) {
-            return builder;
-        } else {
-            return builder.proxyConfiguration(proxyConfig);
-        }
+        return isProxyConfigured() ? builder.proxyConfiguration(proxy.build()) : builder;
     }
 
     /**
@@ -57,5 +52,10 @@ public class NettyClientConfiguration extends AWSConfiguration {
      */
     public ProxyConfiguration.Builder getProxy() {
         return proxy;
+    }
+
+    boolean isProxyConfigured() {
+        ProxyConfiguration proxyConfig = proxy.build();
+        return proxyConfig.host() != null;
     }
 }
