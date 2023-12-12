8352f9bf98e505b0c234f66a49a543172f25b15f
Tim Yates <tim.yates@gmail.com>
2023-12-12 11:51:46 +0000

    Add jupiter engine to get tests running (#1983)

--- function-aws-test/build.gradle.kts
+++ function-aws-test/build.gradle.kts
@@ -6,6 +6,10 @@ dependencies {
     implementation(mnTest.micronaut.test.junit5)
     api(projects.micronautFunctionAws)
     api(mn.micronaut.function)
+
     testAnnotationProcessor(mn.micronaut.inject.java)
+
+    testRuntimeOnly(libs.junit.jupiter.engine)
     testRuntimeOnly(mn.snakeyaml)
+    testRuntimeOnly(mnSerde.micronaut.serde.jackson)
 }
--- function-aws-test/src/test/java/io/micronaut/function/aws/test/CustomBuilderTest.java
+++ function-aws-test/src/test/java/io/micronaut/function/aws/test/CustomBuilderTest.java
@@ -3,20 +3,26 @@ package io.micronaut.function.aws.test;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.DefaultApplicationContextBuilder;
 import io.micronaut.function.aws.test.annotation.MicronautLambdaTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
-import jakarta.inject.Inject;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautLambdaTest(contextBuilder = CustomBuilderTest.CustomBuilder.class)
-public class CustomBuilderTest {
+class CustomBuilderTest {
+
     @Inject
     ApplicationContext context;
 
     @Test
-    public void testContextProperlyConfigured() {
+    void testContextProperlyConfigured() {
         Set<String> expectedNames = new HashSet<>(Arrays.asList("test", "function", "lambda", "custom-env"));
         assertEquals(context.getEnvironment().getActiveNames(), expectedNames);
         assertEquals(Optional.of("baz"), context.getProperty("test.first", String.class));
