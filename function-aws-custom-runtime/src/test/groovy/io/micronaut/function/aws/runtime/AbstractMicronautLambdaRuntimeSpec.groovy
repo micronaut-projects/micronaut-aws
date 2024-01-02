a0e1b9ecccd3c2ac07cf0ed48462ce36a49a5cf7
Sergio del Amo <sergio.delamo@softamo.com>
2024-01-02 17:59:40 +0100

    increase test coverage

--- function-aws-custom-runtime/src/test/groovy/io/micronaut/function/aws/runtime/AbstractMicronautLambdaRuntimeSpec.groovy
+++ function-aws-custom-runtime/src/test/groovy/io/micronaut/function/aws/runtime/AbstractMicronautLambdaRuntimeSpec.groovy
@@ -3,7 +3,11 @@ package io.micronaut.function.aws.runtime
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import io.micronaut.context.exceptions.ConfigurationException
+import io.micronaut.http.HttpHeaders
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.MediaType
 import spock.lang.Specification
+
 
 class AbstractMicronautLambdaRuntimeSpec extends Specification {
 
@@ -16,6 +20,20 @@ class AbstractMicronautLambdaRuntimeSpec extends Specification {
 
         then:
         thrown(ConfigurationException)
+
+        and:
+        null == customRuntime.createRequestHandler()
+
+        and:
+        null == customRuntime.createRequestStreamHandler()
+
+        when:
+        APIGatewayProxyResponseEvent responseEvent = customRuntime.respond(HttpStatus.I_AM_A_TEAPOT, "{\"foo\":\"bar\"}".getBytes(), MediaType.APPLICATION_JSON_GITHUB)
+        then:
+        responseEvent.isBase64Encoded
+        418 == responseEvent.statusCode
+        MediaType.APPLICATION_JSON_GITHUB == responseEvent.getHeaders().get(HttpHeaders.CONTENT_TYPE)
+        new String(Base64.encoder.encode("{\"foo\":\"bar\"}".getBytes())) == responseEvent.body
     }
     
     static class CustomRuntime extends AbstractMicronautLambdaRuntime<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent, APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
