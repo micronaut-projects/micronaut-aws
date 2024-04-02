4dbdaccd9eca2422e49b4738bbf8d447d14ef020
Dean Wette <wetted@unityfoundation.io>
2024-04-02 10:37:06 -0500

    disable JsonErrorTest and VndErrorTest

--- test-suite-http-server-tck-function-aws-api-gateway-proxy-payloadv1/src/test/java/io/micronaut/http/server/tck/lambda/tests/FunctionAwsApiGatewayProxyV1HttpServerTestSuite.java
+++ test-suite-http-server-tck-function-aws-api-gateway-proxy-payloadv1/src/test/java/io/micronaut/http/server/tck/lambda/tests/FunctionAwsApiGatewayProxyV1HttpServerTestSuite.java
@@ -12,7 +12,9 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 })
 @ExcludeClassNamePatterns({
         "io.micronaut.http.server.tck.tests.filter.options.OptionsFilterTest",
-        "io.micronaut.http.server.tck.tests.FilterProxyTest" // Immmutable request
+        "io.micronaut.http.server.tck.tests.FilterProxyTest", // Immmutable request
+        "io.micronaut.http.server.tck.tests.hateoas.JsonErrorTest",
+        "io.micronaut.http.server.tck.tests.hateoas.VndErrorTest"
 })
 @SuiteDisplayName("HTTP Server TCK for Function AWS API Gateway Proxy v1 Event model")
 public class FunctionAwsApiGatewayProxyV1HttpServerTestSuite {
