6fb0c0e0d0a524c51690473f47665cbc8a924e71
Luís Duarte <lduarte@luisduarte.net>
2024-07-03 16:14:09 +0000

    Fix lambda code

--- function-client-aws-v2/src/test/resources/lambda/index.js
+++ function-client-aws-v2/src/test/resources/lambda/index.js
@@ -1,37 +1,17 @@
-/*
-Contract:
-
-interface ComplexObject {
-   aNumber int
-   aString string
-}
-
-interface Request {
-   aNumber int
-   aString string
-   aObject ComplexObject
-}
-
-interface Response {
-   aNumber int
-   aString string
-   aObject ComplexObject
-   anArray []ComplexObject
-}
-*/
-
-exports.handler = async function (event, context) {
+exports.handler = async (event, context) => {
     if (!event.aNumber || !event.aString || !event.aObject || !event.aObject.aNumber || !event.aObject.aString) {
       throw new Error('Invalid Input');
     }
 
-    var arr = [];
+    const arr = [];
     arr.push(event.aObject);
 
-    return {
-      aNumber: event.aNumber,
-      aString: event.aString,
-      aObject: event.aObject,
-      anArray: arr,
+    const response = {
+        aNumber: event.aNumber,
+        aString: event.aString,
+        aObject: event.aObject,
+        anArray: arr,
     };
+
+    return response
 };
