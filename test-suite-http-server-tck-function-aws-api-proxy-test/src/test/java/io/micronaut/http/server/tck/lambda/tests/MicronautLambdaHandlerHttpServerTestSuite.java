c3d3645d85dc0d1567d4e8465f3d57a70d928736
Sergio del Amo <sergio.delamo@softamo.com>
2024-11-07 13:30:06 +0100

    ignore FormUrlEncodedBodyInRequestFilterTest

--- test-suite-http-server-tck-function-aws-api-proxy-test/src/test/java/io/micronaut/http/server/tck/lambda/tests/MicronautLambdaHandlerHttpServerTestSuite.java
+++ test-suite-http-server-tck-function-aws-api-proxy-test/src/test/java/io/micronaut/http/server/tck/lambda/tests/MicronautLambdaHandlerHttpServerTestSuite.java
@@ -11,6 +11,7 @@ import org.junit.platform.suite.api.SuiteDisplayName;
     "io.micronaut.http.server.tck.lambda.tests"
 })
 @ExcludeClassNamePatterns({
+        "io.micronaut.http.server.tck.tests.forms.FormUrlEncodedBodyInRequestFilterTest",
         "io.micronaut.http.server.tck.tests.jsonview.JsonViewsTest", // https://github.com/micronaut-projects/micronaut-servlet/pull/826
         "io.micronaut.http.server.tck.tests.forms.FormsSubmissionsWithListsTest",
         "io.micronaut.http.server.tck.tests.filter.options.OptionsFilterTest",
