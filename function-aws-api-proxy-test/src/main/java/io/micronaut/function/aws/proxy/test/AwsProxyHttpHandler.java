da0bb7152426fa12a745d635126ae155b2dd4aba
Sergio del Amo <sergio.delamo@softamo.com>
2025-03-24 10:06:31 +0100

    remove extra blank line

--- function-aws-api-proxy-test/src/main/java/io/micronaut/function/aws/proxy/test/AwsProxyHttpHandler.java
+++ function-aws-api-proxy-test/src/main/java/io/micronaut/function/aws/proxy/test/AwsProxyHttpHandler.java
@@ -39,7 +39,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
-
 @Internal
 class AwsProxyHttpHandler implements HttpHandler {
     APIGatewayV2HTTPEventFunction handler;
