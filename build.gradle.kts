08b22b9f4f43e83552f663bbeec56a88d27a9a90
issam liqali <63265720+lissam1@users.noreply.github.com>
2025-09-09 14:14:17 +0100

    update micronaut build to 7.6.4 (#2400)
    * update micronaut build to 7.6.4
    
    * build: use publishToMavenCentral

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    id("io.micronaut.build.internal.docs")
+    id("io.micronaut.build.internal.parent")
     id("io.micronaut.build.internal.dependency-updates")
-    id("io.micronaut.build.internal.quality-reporting")
 }
 
 repositories {
