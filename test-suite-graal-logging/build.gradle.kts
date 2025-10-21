7073bf00622285c9849649beba5a9080b5bb68d4
Sergio del Amo <sergio.delamo@softamo.com>
2025-10-21 10:14:15 +0200

    Update test-suite-graal-logging/build.gradle.kts

--- test-suite-graal-logging/build.gradle.kts
+++ test-suite-graal-logging/build.gradle.kts
@@ -12,28 +12,3 @@ micronaut {
     runtime("netty")
     testRuntime("junit5")
 }
-
-graalvmNative {
-    binaries {
-        all {
-            buildArgs.add("--trace-class-initialization=org.slf4j.LoggerFactory")
-
-            buildArgs.add("--initialize-at-build-time=org.junit.platform.engine.support.store.NamespacedHierarchicalStore\$EvaluatedValue")
-            buildArgs.add("--initialize-at-build-time=org.junit.platform.launcher.core.LauncherPhase")
-            buildArgs.add("--initialize-at-build-time=org.junit.platform.launcher.core.DiscoveryIssueNotifier")
-            buildArgs.add("--initialize-at-build-time=org.junit.platform.launcher.core.HierarchicalOutputDirectoryCreator")
-        }
-    }
-
-graalvmNative {
-    binaries {
-        all {
-            buildArgs.add("--trace-class-initialization=org.slf4j.LoggerFactory")
-
-            buildArgs.add("--initialize-at-build-time=org.junit.platform.engine.support.store.NamespacedHierarchicalStore\$EvaluatedValue")
-            buildArgs.add("--initialize-at-build-time=org.junit.platform.launcher.core.LauncherPhase")
-            buildArgs.add("--initialize-at-build-time=org.junit.platform.launcher.core.DiscoveryIssueNotifier")
-            buildArgs.add("--initialize-at-build-time=org.junit.platform.launcher.core.HierarchicalOutputDirectoryCreator")
-        }
-    }
-}
