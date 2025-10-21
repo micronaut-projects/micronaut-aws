feeec8c1c45ad746237e652e7558d8114231e241
Sergio del Amo <sergio.delamo@softamo.com>
2025-10-21 10:13:48 +0200

    Update test-suite-graal-logging/build.gradle.kts

--- test-suite-graal-logging/build.gradle.kts
+++ test-suite-graal-logging/build.gradle.kts
@@ -24,4 +24,16 @@ graalvmNative {
             buildArgs.add("--initialize-at-build-time=org.junit.platform.launcher.core.HierarchicalOutputDirectoryCreator")
         }
     }
+
+graalvmNative {
+    binaries {
+        all {
+            buildArgs.add("--trace-class-initialization=org.slf4j.LoggerFactory")
+
+            buildArgs.add("--initialize-at-build-time=org.junit.platform.engine.support.store.NamespacedHierarchicalStore\$EvaluatedValue")
+            buildArgs.add("--initialize-at-build-time=org.junit.platform.launcher.core.LauncherPhase")
+            buildArgs.add("--initialize-at-build-time=org.junit.platform.launcher.core.DiscoveryIssueNotifier")
+            buildArgs.add("--initialize-at-build-time=org.junit.platform.launcher.core.HierarchicalOutputDirectoryCreator")
+        }
+    }
 }
