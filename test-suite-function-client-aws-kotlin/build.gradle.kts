f101f86a22043927d0f16f3e3675e59af4f6dd7a
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-02 14:51:30 +0100

    define source and target compatibility

--- test-suite-function-client-aws-kotlin/build.gradle.kts
+++ test-suite-function-client-aws-kotlin/build.gradle.kts
@@ -23,7 +23,10 @@ tasks {
         useJUnitPlatform()
     }
 }
-
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
