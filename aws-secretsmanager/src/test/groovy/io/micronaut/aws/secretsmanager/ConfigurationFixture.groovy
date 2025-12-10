43c17d19fdda1fb6ca7025bafb038b0e3778bec7
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-10 17:01:52 +0100

    fix configuration fixture

--- aws-secretsmanager/src/test/groovy/io/micronaut/aws/secretsmanager/ConfigurationFixture.groovy
+++ aws-secretsmanager/src/test/groovy/io/micronaut/aws/secretsmanager/ConfigurationFixture.groovy
@@ -4,6 +4,7 @@ interface ConfigurationFixture {
 
     default Map<String, Object> getConfiguration() {
         Map<String, Object> m = [:]
+        String specName = getSpecName()
         if (specName) {
             m['spec.name'] = specName
         }
