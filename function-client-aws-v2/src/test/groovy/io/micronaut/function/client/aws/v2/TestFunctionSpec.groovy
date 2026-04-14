92c1bd5e5c9043b576f11ce7f13d64c5e9d2abc7
yawkat <jonas.konrad@oracle.com>
2026-04-14 08:47:34 +0000

    Skip LocalStack AWS tests without Docker
    Co-Authored-By: multicode <multicode@yawk.at>

--- function-client-aws-v2/src/test/groovy/io/micronaut/function/client/aws/v2/TestFunctionSpec.groovy
+++ function-client-aws-v2/src/test/groovy/io/micronaut/function/client/aws/v2/TestFunctionSpec.groovy
@@ -29,6 +29,7 @@ import software.amazon.awssdk.services.lambda.model.GetFunctionConfigurationRequ
 import software.amazon.awssdk.services.lambda.model.Runtime
 import software.amazon.awssdk.services.lambda.model.GetFunctionRequest
 import software.amazon.awssdk.services.lambda.model.LambdaRequest
+import spock.lang.IgnoreIf
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -38,6 +39,7 @@ import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
 @Testcontainers
+@IgnoreIf({ !Boolean.getBoolean("test.docker.available") })
 @MicronautTest
 class TestFunctionSpec extends Specification implements TestPropertyProvider {
 
