ac73af71290e0da69108a1746caebb9feee3ad95
Sergio del Amo <sergio.del.amo.caballero@oracle.com>
2026-06-30 11:49:30 +0200

    add getAll

--- function-aws-api-proxy/src/test/java/io/micronaut/function/aws/proxy/payload2/APIGatewayV2HTTPEventFunctionTest.java
+++ function-aws-api-proxy/src/test/java/io/micronaut/function/aws/proxy/payload2/APIGatewayV2HTTPEventFunctionTest.java
@@ -165,6 +165,14 @@ class APIGatewayV2HTTPEventFunctionTest {
     }
 
     @Test
+    void cookieHeaderSingleValueAccessPreservesAllCookiesWhenBindingToHeader() throws IOException {
+        executeTest("/cookieHeader", response -> {
+            assertEquals("""
+                ["cookie1=value1","cookie2=value2"]""", response.getBody());
+        });
+    }
+
+    @Test
     void v2ResponseMissesCookieInHeader() throws IOException {
         executeTest("/cookies", response -> {
             assertEquals(Set.of("cookie1=value1", "cookie2=value2"), Set.copyOf(response.getCookies()));
@@ -220,6 +228,11 @@ class APIGatewayV2HTTPEventFunctionTest {
         @Post("/cookiesObject")
         List<String> cookiesObject(Cookies cookies) {
             return serverCookieEncoder.encode(cookies.get("cookie1"), cookies.get("cookie2"));
+        }
+
+        @Post("/cookieHeader")
+        List<String> cookieHeader(HttpRequest<?> request) {
+            return request.getHeaders().getAll(HttpHeaders.COOKIE);
         }
     }
 }
