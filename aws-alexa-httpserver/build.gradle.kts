7eba06c4260e50be9c1b18c373184cd96e1870e0
Sergio del Amo <sergio.del.amo.caballero@oracle.com>
2026-06-30 08:38:37 +0200

    fix cves

--- aws-alexa-httpserver/build.gradle.kts
+++ aws-alexa-httpserver/build.gradle.kts
@@ -12,8 +12,8 @@ dependencies {
         exclude(group = "com.fasterxml.jackson.core", module = "jackson-core")
     }
     constraints {
-        // " com.amazon.alexa:ask-sdk-core 2.86.0 requests jackson-core 2.13.0, which is affected by GHSA-72hv-8253-57qq"
-        api("com.fasterxml.jackson.core:jackson-core:2.18.6") {
+        // " com.amazon.alexa:ask-sdk-core 2.86.0 requests jackson-core 2.13.0, which is affected by several CVEs"
+        api("com.fasterxml.jackson.core:jackson-core:2.18.8") {
             because("Require a non-vulnerable jackson-core version instead of the transitive version")
         }
     }
