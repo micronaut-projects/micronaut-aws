cfb8ef2ae8b2bc292997572784a3b75b508edffc
Issam Liqali <liqaliissam@gmail.com>
2026-07-13 13:02:34 +0100

    Fix duplicate embedded server startup for lambda_java runtime (#2552)
    * Fix duplicate embedded server startup for lambda_java runtime
    
    Closes : #2511
    
    remove unnecesary @Property micronaut.server.port

--- function-aws-api-proxy-test/src/main/java/io/micronaut/function/aws/proxy/test/EmbeddedServerFactory.java
+++ function-aws-api-proxy-test/src/main/java/io/micronaut/function/aws/proxy/test/EmbeddedServerFactory.java
@@ -20,10 +20,13 @@ import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.ApplicationContextBuilder;
 import io.micronaut.context.ApplicationContextProvider;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.env.Environment;
 import io.micronaut.context.env.PropertySource;
 import io.micronaut.context.exceptions.ConfigurationException;
 import io.micronaut.core.annotation.Experimental;
 import io.micronaut.core.annotation.Internal;
+import io.micronaut.function.aws.MicronautLambdaContext;
 import io.micronaut.function.aws.proxy.payload2.APIGatewayV2HTTPEventFunction;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
@@ -31,12 +34,14 @@ import jakarta.inject.Singleton;
 @Experimental
 @Internal
 @Factory
+@Requires(notEnv = Environment.FUNCTION)
 class EmbeddedServerFactory {
 
     @Named("HttpServer")
     @Singleton
     ApplicationContextProvider httpServerApplicationContextProvider(ApplicationContext applicationContext) {
-        ApplicationContextBuilder builder = ApplicationContext.builder();
+        ApplicationContextBuilder builder = ApplicationContext.builder()
+            .environments(Environment.FUNCTION, MicronautLambdaContext.ENVIRONMENT_LAMBDA);
         for (PropertySource propertySource : applicationContext.getEnvironment().getPropertySources()) {
             builder = builder.propertySources(propertySource);
         }
